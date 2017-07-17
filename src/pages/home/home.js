2b8008276379d5fb9839de636d404d17bd255de8
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2017-07-17 12:35:26 +0300

    swipe projects left to right

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,7 +2,11 @@ import homeHtml from './home.html';
 import homeScss from './home.scss';
 import projectsObject from './projects';
 
-let homeObject, rendered, initialHTML;
+let homeObject, rendered, initialHTML, projectElements;
+let currentActiveElementIndex = 0;
+const projectActiveClass = 'project--displayed';
+const projectHiddenLeft = 'project--hidden-to-left';
+const projectHiddenRight = 'project--hidden-to-right';
 
 function renderMustache(initialHtml, contentToRender, template) {
     Mustache.parse(initialHTML);   // optional, speeds up future uses
@@ -10,14 +14,73 @@ function renderMustache(initialHtml, contentToRender, template) {
     template.innerHTML = rendered;
 }
 
+function removeHiddenClasses(itemList) {
+    itemList[currentActiveElementIndex].classList.remove(projectHiddenLeft, projectHiddenRight);
+}
+
+function addActiveClass(itemList) {
+    removeHiddenClasses(itemList);
+    itemList[currentActiveElementIndex].classList.add(projectActiveClass);
+}
+
+function hidePreviousActiveElement(itemList, currentActiveElementIndex, direction) {
+    clearActiveClass(itemList, projectActiveClass);
+    if(direction === 'previous') {
+        itemList[currentActiveElementIndex].classList.add(projectHiddenRight);
+    }
+    else {
+        itemList[currentActiveElementIndex].classList.add(projectHiddenLeft);
+    }
+}
+
+function changeActiveElement(itemList, direction) {
+    let projectItemsLength = itemList.length;
+    if(direction === 'previous') {
+        if(currentActiveElementIndex === 0) {
+            currentActiveElementIndex = projectItemsLength-1;
+        }
+        else {
+            currentActiveElementIndex--;
+        }
+    }
+    else if(direction === 'next') {
+        if(currentActiveElementIndex === projectItemsLength-1) {
+            currentActiveElementIndex = 0;
+        }
+        else {
+            currentActiveElementIndex++;
+        }
+    }
+    addActiveClass(itemList);
+}
+
+function clearActiveClass(itemList, classToRemove) {
+    for(const item of itemList) {
+        item.classList.remove(classToRemove);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-    homeObject = document.getElementById('home');
+    homeObject = document.getElementsByClassName('projects')[0];
     initialHTML = homeObject.innerHTML;
-    renderMustache(initialHTML, {projects: projectsObject[language]}, homeObject)
+    renderMustache(initialHTML, {projects: projectsObject[language]}, homeObject);
+    projectElements = document.getElementsByClassName('project');
+    projectElements[0].classList.add(projectActiveClass);
+    projectElements[0].classList.remove(projectHiddenRight);
+    const projectNavButtons = document.getElementsByClassName('project-navigation')[0].children;
+    for(const projectButton of projectNavButtons) {
+        const direction = projectButton.getAttribute('data-direction');
+        projectButton.addEventListener('click', function(){
+            hidePreviousActiveElement(projectElements, currentActiveElementIndex, direction);
+            changeActiveElement(projectElements, direction)
+        });
+    }
+    
 });
 
 document.addEventListener('changeLang', function(){
     renderMustache(initialHTML, {projects: projectsObject[language]}, homeObject)
+    addActiveClass(projectElements);
 });
 
 module.exports = {
--- src/pages/home/projects.js
+++ src/pages/home/projects.js
@@ -1,9 +1,17 @@
 import benchmarkImg from './images/projects/benchmark.jpg';
+import capitalFortImg from './images/projects/capital-fort-sofia.jpg';
 
 const englishProjects = [
     {
         image: benchmarkImg,
         name: 'Benchmark',
+        services: [
+            'SKS', 'Access Control', 'Fire alarm systems', 'Surveillance'
+        ]
+    },
+    {
+        image: capitalFortImg,
+        name: 'Capital Fort',
         services: [
             'SKS', 'Access Control', 'Fire alarm systems', 'Surveillance'
         ]
@@ -14,6 +22,13 @@ const bulgarianProjects = [
     {
         image: benchmarkImg,
         name: 'Бенчмарк',
+        services: [
+            'СКС', 'Контрол на достъп', 'Пожароизвестяване', 'Видео наблюдение'
+        ]
+    },
+    {
+        image: capitalFortImg,
+        name: 'Кепитал Форт',
         services: [
             'СКС', 'Контрол на достъп', 'Пожароизвестяване', 'Видео наблюдение'
         ]
