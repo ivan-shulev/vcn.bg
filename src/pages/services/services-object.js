1a75d5e3e77369e83dc537b0bda82c94d73b6cc5
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2017-08-03 23:50:35 +0300

    services page

---
+++ src/pages/services/services-object.js
@@ -0,0 +1,26 @@
+const english = {
+    body: 'Design, implementation and <strong>launching</strong> of',
+    services: [
+        'Access Control',
+        'Fire alarm systems',
+        'Public address systems',
+        'Surveillance'
+    ]
+};
+
+const bulgarian = {
+    body: 'Проектиране, изграждане и <strong>пускове</strong> на',
+    services: [
+        'Контрол на достъп',
+        'Пожароизвестяване',
+        'Озвучаване и оповестяване',
+        'Видеонаблюдение'
+    ]
+};
+
+const servicesObject = {
+    english,
+    bulgarian
+};
+
+module.exports = servicesObject;
---
+++ src/pages/services/services.js
@@ -0,0 +1,7 @@
+import servicesHtml from './services.html';
+import servicesScss from './services.scss';
+import servicesObject from './services-object';
+
+module.exports = {
+    servicesHtml
+};
