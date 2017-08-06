65856dd5bbde65a3984ecd1a46fdac5d06881dbc
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2017-08-06 16:15:40 +0300

    keep state of route and change-lang function

---
+++ src/utils/change-lang.js
@@ -0,0 +1,23 @@
+let _changeLangFunc, _route;
+
+function changeLang() {
+    return _changeLangFunc();
+}
+
+function isNewRoute(route) {
+    return _route !== route;
+}
+
+module.exports = function setChangeLang(changeLangFunc, route) {
+    _changeLangFunc = changeLangFunc;
+    
+    // _route is undefined at first
+    if(_route && isNewRoute(route)) {
+        document.removeEventListener('changeLang', changeLang);
+        _route = route;
+    }
+    else if (!_route) {
+        _route = route;
+    }
+    document.addEventListener('changeLang', changeLang);
+};
