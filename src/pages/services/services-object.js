dc4e8eaecc1e32912a480fd7b99bf66f567a9f99
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2017-08-06 18:58:38 +0300

    add translations for services

--- src/pages/services/services-object.js
+++ src/pages/services/services-object.js
@@ -1,21 +1,29 @@
 const english = {
-    body: 'Design, implementation and <strong>launching</strong> of',
-    services: [
+    header: 'We are very good at',
+    firstRow: 'Design, implementation and <strong>launch</strong> of',
+    servicesArray: [
         'Access Control',
         'Fire alarm systems',
         'Public address systems',
         'Surveillance'
-    ]
+    ],
+    secondRow: 'Design, implementation and certification of SKS',
+    thirdRow: 'Consulting Services',
+    fourthRow: 'Maintenance of built systems'
 };
 
 const bulgarian = {
-    body: 'Проектиране, изграждане и <strong>пускове</strong> на',
-    services: [
+    header: 'Много сме добри с',
+    firstRow: 'Проектиране, изграждане и <strong>пускове</strong> на',
+    servicesArray: [
         'Контрол на достъп',
         'Пожароизвестяване',
         'Озвучаване и оповестяване',
         'Видеонаблюдение'
-    ]
+    ],
+    secondRow: 'Проектиране, изграждане и сертифициране на СКС',
+    thirdRow: 'Консултантски услуги',
+    fourthRow: 'Поддръжка на изградените системи'
 };
 
 const servicesObject = {
--- src/pages/services/services.js
+++ src/pages/services/services.js
@@ -2,16 +2,22 @@ import servicesHtml from './services.html';
 import servicesScss from './services.scss';
 import servicesObject from './services-object';
 import setChangeLang from '../../utils/change-lang';
+import renderMustache from '../../utils/render-mustache';
 
-const changeLang = function () {
-    console.log('services ChangeLang');
+let servicesContainer, initialHTML;
+
+const renderHtml = function () {
+    renderMustache(initialHTML, { content: servicesObject[language] }, servicesContainer);
 }
 
 document.addEventListener('changeRoute', function (e) {
     if(e.detail !== 'services') {
         return;
     }
-    setChangeLang(changeLang, 'services');
+    servicesContainer = document.querySelector('.services');
+    initialHTML = servicesContainer.innerHTML;
+    renderHtml();
+    setChangeLang(renderHtml, 'services');
 });
 
 module.exports = {
