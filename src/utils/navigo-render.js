f24ac50aed60a50b8a35ec25b5923cc9afdcebbe
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2017-11-20 20:35:59 +0200

    fix a bug with double navigo calls

--- src/utils/navigo-render.js
+++ src/utils/navigo-render.js
@@ -4,14 +4,17 @@ let _router;
 const bodyElement = document.querySelector('body');
 
 function renderNavigoLinks() {
-    const navigoLinks = Array.from(document.querySelectorAll('[data-navigo]'));
-    navigoLinks.forEach(link => {
-        link.addEventListener(clickEvent, (e) => {
+    const navigoLinkHolders = document.querySelectorAll('[data-main-navigation-links]');
+    Array.from(navigoLinkHolders).forEach(holder => {
+        holder.addEventListener(clickEvent, e => {
+            if(e.target.tagName !== 'A') {
+                return;
+            }
             e.preventDefault();
             bodyElement.classList.remove('menu--open');
             _router.navigate(e.target.getAttribute('href'));
-        })
-    });
+        });
+    })
 }
 
 function navigoReRender() {
